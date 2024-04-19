03f5387d3a2d77abb99673096356460a7976bc5e
Guilherme Bedan <87638668+guibedan@users.noreply.github.com>
2024-04-19 01:10:08 -0300

    second

--- app/src/main/java/com/example/lotrias/MainActivity.kt
+++ app/src/main/java/com/example/lotrias/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.lotrias
 
+import android.app.AlertDialog
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private var isAnimating = false
     private var randomNumber: Int = 0
     private var handler = Handler(Looper.getMainLooper())
+    private var mediaPlayer = MediaPlayer();
+    private var dbHelper: DBSQLite? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +29,46 @@ class MainActivity : AppCompatActivity() {
         val textView5 = findViewById<TextView>(R.id.textView5)
         val textView6 = findViewById<TextView>(R.id.textView6)
 
-        // armazena em uma variavei a referencia do botão
+        // Cria a variavel que armazena a música
+        mediaPlayer = MediaPlayer.create(this, R.raw.piao)
+
+        // armazena em variaveis as referencias dos botões
         val buttonGenerate = findViewById<Button>(R.id.button)
+        val alert = findViewById<Button>(R.id.alert)
+
+        alert.setOnClickListener {
+            // Crie uma instância de LotteryDatabaseHelper para gerenciar o banco de dados
+            val dbHelper = DBSQLite(this)
+
+            // Obtenha uma referência ao banco de dados para leitura
+            val db = dbHelper.readableDatabase
+
+            // Realize a consulta para obter os números da tabela `loteria_nums`
+            val lotteryNumbersList = selectLotteryNumbers(db)
+
+            // Feche o banco de dados após a consulta
+            db.close()
+
+            // Crie uma string para exibir os números
+            val numbersString = lotteryNumbersList.joinToString("\n")
+
+            // Crie um alert dialog
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Números da Loteria")
+            builder.setMessage(numbersString)
+            builder.setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            // Exiba o alert dialog
+            builder.create().show()
+        }
 
         // Seta um método que espera uma interação de click com o botão
         buttonGenerate.setOnClickListener {
+            mediaPlayer.start()
+            dbHelper = DBSQLite(this)
+
             // Gera um número aleatório para cada variavel
             if (!isAnimating) {
                 startAnimation(textView1, textView2, textView3, textView4, textView5, textView6)
@@ -41,6 +80,7 @@ class MainActivity : AppCompatActivity() {
         isAnimating = true
         handler.postDelayed(object : Runnable {
             override fun run() {
+
                 // Gera um número aleatório para cada variável
                 randomNumber = (1..99).random()
                 val randomNumber1 = (1..99).random()
@@ -63,7 +103,16 @@ class MainActivity : AppCompatActivity() {
 
                 // Condição de parada da animação (pode ser qualquer condição desejada)
                 if (randomNumber == 50) {
+                    // Para a música
+                    if (mediaPlayer.isPlaying) mediaPlayer.pause();
+                    // Para a animação
                     stopAnimation()
+                    // Salva no Banco de dados
+                    val list: List<Int> = listOf(randomNumber1, randomNumber2, randomNumber3, randomNumber4, randomNumber5, randomNumber6)
+                    var db = dbHelper?.writableDatabase
+                    if (db != null) {
+                        insertLotteryNumbers(db, randomNumber1, randomNumber2, randomNumber3, randomNumber4, randomNumber5, randomNumber6)
+                    };
                 }
             }
         }, 0)
